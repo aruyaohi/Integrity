79e10390dad5815e44409232bd06a0bbfcbcc0be
aruyaohi <omariontechnologies@gmail.com>
2025-06-01 23:20:42 +0100

    added github link toactual project

--- page_reviewer/src/app/page.tsx
+++ page_reviewer/src/app/page.tsx
@@ -32,15 +32,12 @@ const AnimatedHeader = () => {
   // }
 
   const mobileNav = [
-    {name: 'Gihub', icon: Github, href: '#'},
+    {name: 'Gihub', icon: Github, href: 'https://github.com/aruyaohi/academic_paper_reviewer'},
     {name: 'Docs', icon: ScrollIcon, href: '#'}
   ]
 
    const socialLinks = [
-    // { icon: Twitter, href: 'https://twitter.com', label: 'Twitter' },
-    { icon: Github, href: '#', label: 'GitHub' },
-    // { icon: Linkedin, href: 'https://linkedin.com', label: 'LinkedIn' },
-    // { icon: Mail, href: 'mailto:contact@peerreview.com', label: 'Email' }
+    { icon: Github, href: 'https://github.com/aruyaohi/academic_paper_reviewer', label: 'GitHub' },
   ];
 
   const toggleMobileMenu = () => {
